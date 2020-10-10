03229f84cd3ea4cf5c8fa1b526c190a256ad7543
Kamran <balayev.kamran@mail.ru>
2020-10-10 21:46:26 +0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,75 @@
+# Writing to an excel  
+# sheet using Python 
+import xlwt 
+from xlwt import Workbook 
+import xlsxwriter 
+
+
+
+workbook = xlsxwriter.Workbook('dialogflowResults.xlsx')
+worksheet = workbook.add_worksheet()
+
+
+def detect_intent_texts(project_id, session_id, texts, language_code):
+
+    """Returns the result of detect intent with texts as inputs.
+
+    Using the same `session_id` between requests allows continuation
+    of the conversation."""
+    import dialogflow_v2 as dialogflow
+    session_client = dialogflow.SessionsClient()
+
+    session = session_client.session_path(project_id, session_id)
+    print('Session path: {}\n'.format(session))
+    row=1
+    
+    worksheet.write(0, 0, "CAT_ID") 
+    worksheet.write(0, 1, "CAT_NAME") 
+    worksheet.write(0, 2, "TRAINING_DATA") 
+    worksheet.write(0, 3, "SCORE") 
+    
+    for text in texts:
+        text_input = dialogflow.types.TextInput(
+            text=text, language_code=language_code)
+
+        query_input = dialogflow.types.QueryInput(text=text_input)
+        response = session_client.detect_intent(
+            session=session, query_input=query_input)
+
+        print('=' * 20)
+        print('Query text: {}'.format(response.query_result.query_text))
+        print('Detected intent: {} (confidence: {})\n'.format(
+            response.query_result.intent.display_name,
+            response.query_result.intent_detection_confidence))
+        print('Fulfillment text: {}\n'.format(
+            response.query_result.fulfillment_text))
+            
+        
+        worksheet.write(row, 0, response.query_result.intent.display_name) 
+        worksheet.write(row, 1, response.query_result.fulfillment_text) 
+        worksheet.write(row, 2,response.query_result.query_text) 
+        worksheet.write(row, 3,response.query_result.intent_detection_confidence) 
+        row+=1
+        
+    workbook.close()
+        
+import pandas as pd
+
+project_id = "newagent-croe"
+
+excelFile = pd.read_excel (r"C:/Users/balay/OneDrive/Masaüstü/dialogflow/testDatas.xlsx")
+
+queries=[]
+counter=0
+for i in excelFile.iterrows():
+    queries.append(i[1].testdata)
+    counter=counter+1
+    if counter== 186:
+        detect_intent_texts(project_id, "123456789", queries, "tr-TR");
+        queries.clear()
+        counter=0
+        break
+
+   
+    
+   
---
+++ train.py
@@ -0,0 +1,92 @@
+def create_intent(project_id, display_name, training_phrases_parts,
+                  message_texts):
+    """Create an intent of the given intent type."""
+    import dialogflow_v2 as dialogflow
+    intents_client = dialogflow.IntentsClient()
+
+    parent = intents_client.project_agent_path(project_id)
+    training_phrases = []
+    for training_phrases_part in training_phrases_parts:
+        part = dialogflow.types.Intent.TrainingPhrase.Part(
+            text=training_phrases_part)
+        # Here we create a new training phrase for each provided part.
+        training_phrase = dialogflow.types.Intent.TrainingPhrase(parts=[part])
+        training_phrases.append(training_phrase)
+
+    text = dialogflow.types.Intent.Message.Text(text=message_texts)
+    message = dialogflow.types.Intent.Message(text=text)
+
+    intent = dialogflow.types.Intent(
+        display_name=display_name,
+        training_phrases=training_phrases,
+        messages=[message])
+
+    response = intents_client.create_intent(parent, intent)
+
+    print('Intent created: {}'.format(response))
+    
+ 
+import pandas as pd
+
+project_id = "newagent-croe"
+
+excelFile = pd.read_excel (r"C:/Users/balay/OneDrive/Masaüstü/Train split.xlsx")
+
+
+index=1#index value for excel
+#arrays for storing values in order to create intent
+training_phrases_parts=[]
+display_name=['5'] #initial value for id
+message_texts=[]
+
+counter=0
+nameCount=0
+
+
+#iterate untill the end of excel file
+for i in excelFile.iterrows():
+    #add training phrase to array
+    training_phrases_parts.append(i[index].TRAINING_DATA)
+    # assign index value from excel
+    display_name[0]=i[index].CAT_ID
+    #add message texts to array
+    message_texts.append(i[index].CAT_NAME)
+    #counter for repeating operations
+    counter=counter+1
+    #create intents in every 300 iteration    
+    if counter== 300:
+        display_name[0]=str(display_name[0])+"_"+str(nameCount)
+        nameCount=nameCount+1
+        
+        #assign array value to another variable
+        dp=display_name[0]
+        #create intents
+        create_intent(project_id, dp, training_phrases_parts, message_texts)
+        
+        #reset arrays for other iterations
+        training_phrases_parts.clear()
+        message_texts.clear()
+        counter=0
+        
+        #remove last character which is _name counter
+        if nameCount<=9:
+            dp=dp[:-2] 
+        elif nameCount<=99:
+            dp=dp[:-3]    
+        elif nameCount<=999:
+            dp=dp[:-4]
+        elif nameCount<=9999:
+            dp=dp[:-5]
+            
+        display_name[0]=dp
+        
+        
+    
+
+"""
+#control for resetting display name counter
+    if display_name[0]!=i[index].CAT_ID:
+        nameCount=0
+"""
+     
+    
